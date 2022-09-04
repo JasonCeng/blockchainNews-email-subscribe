93e1bc5758105f32748e70e57c85ccc72dd92087
JasonCeng <489281085@qq.com>
2022-09-04 15:21:32 +0800

    update email

--- receivers.py
+++ receivers.py
@@ -2,9 +2,4 @@
 
 MAIL_RECEIVER = [
     "489281085@qq.com",
-    "cengzhaochuang@stu.csust.edu.cn",
-    "zengzc@sdc.icbc.com.cn",
-    "274275954@qq.com",
-    "1397622901@qq.com",
-    "dongshanlingb@163.com",
 ]
